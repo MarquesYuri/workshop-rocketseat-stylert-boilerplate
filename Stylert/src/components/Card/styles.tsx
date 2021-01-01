1e2d054bdba1227e98279c0e265c37f0d2624e87
Marques Yuri de Souza <my.souz4@gmail.com>
2021-01-01 09:53:00 -0300

    Complete Workshop

---
+++ Stylert/src/components/Card/styles.tsx
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components/native'
+import { ICardStyledProps } from './interface'
+
+export const Container = styled.View`
+  height: 72px;
+  padding: 14px;
+  margin: 24px;
+  border-radius: 4px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors['neutral-200'] };
+`
+
+const fontVariant = css`
+  color: ${({ theme }) => theme.colors['neutral-700'] };
+  font-family: ${({ theme }) => theme.fonts.label };
+  font-size: 24px;
+`
+
+export const Label = styled.Text`
+  ${ fontVariant }
+`
+
+export const Description = styled.Text<ICardStyledProps>`
+  ${ fontVariant };
+  color: ${({theme, color}) => theme.colors[color]};
+  ${
+    ({strikeThrough, theme, color}) => 
+      strikeThrough && css`
+        text-decoration: line-through
+        text-decoration-color: ${theme.colors[color]}
+      `
+  }
+`
